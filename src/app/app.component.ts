834028d12ee258b93e8065b1ce8973418ad09945
Abdullah Ibrahim <hello@designcode.me>
2024-09-21 11:46:04 +0200

    deps(webapp): Adds eslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StorageService } from '@core/storage';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'webapp';
+  storageService = inject(StorageService);
+
+  key = Math.random().toString().substring(0, 5);
+  title: string | null = 'Loading';
+
+  constructor() {
+    this.storageService.setItem(this.key, 'Storage Works');
+    this.title = this.storageService.getItem(this.key);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,12 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStorage, withLocalStorage } from '@core/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideStorage(withLocalStorage()),
+  ],
 };
