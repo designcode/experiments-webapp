9157896101a8a7d948c532b2106af9905850552a
Abdullah Ibrahim <hello@designcode.me>
2024-09-23 17:31:34 +0200

    feat(todo): Creates todo list feature lib

--- src/libs/todo/shell/todo.routes.ts
+++ src/libs/todo/shell/todo.routes.ts
@@ -3,5 +3,7 @@ import { Routes } from '@angular/router';
 export const todoRoutes: Routes = [
   {
     path: '',
+    loadChildren: () =>
+      import('@todo/feature-list').then(({ todoListRoutes }) => todoListRoutes),
   },
 ];
