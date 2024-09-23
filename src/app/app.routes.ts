07a6307ec927724b15a40b7804feb35482407a5e
Abdullah Ibrahim <hello@designcode.me>
2024-09-23 17:28:18 +0200

    feat(todo): Creates todo shell lib

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,14 @@ import { DesignSystemComponent, LayoutComponent } from '@ui/components';
 export const routes: Routes = [
   {
     path: '',
+    redirectTo: 'todo',
+    pathMatch: 'full',
+  },
+  {
+    path: 'todo',
     component: LayoutComponent,
+    loadChildren: () =>
+      import('@todo/shell').then(({ todoRoutes }) => todoRoutes),
   },
   {
     path: 'design-system',
